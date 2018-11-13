35848532d5d757f8c14de1bf7b7969f238e82661
Taylor Jackson <tjack94@hotmail.com>
2018-11-12 21:57:33 -0700

    completed exercise 3

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -2,6 +2,13 @@
  * Shuffle an array in place
  * @param a Array to shuffle
  */
+export enum Suit{
+  Clubs, Diamonds, Hearts, Spades
+}
+export enum CardNumber{
+  Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+}
+type Card =[Suit, CardNumber ]
 function shuffleArray(a: any[]) {
   // Iterate over the array
   for (let i = a.length; i; i--) {
@@ -11,7 +18,31 @@ function shuffleArray(a: any[]) {
     [a[i - 1], a[j]] = [a[j], a[i - 1]];
   }
 }
-
-// export class Dealer {
-
-// }
+function createDeck(): Card[]{
+  let cards: Card[]= []
+for(let s= 0; s< Object.keys(Suit).length; s+= 2){
+  for (let n = 0; n < Object.keys(CardNumber).length; n+= 2) {
+   cards.push([s/2, n/2])
+  }
+}
+return cards
+}
+export class Dealer {
+  cards: Card[]=[]
+  constructor(){
+    this.cards = createDeck()
+    shuffleArray(this.cards)
+  }
+dealHand(numCards: number): Card[]{
+  if(numCards > this.getLength()) throw new Error("Not Enough Cards") 
+  if(numCards < 0 ) throw new Error("Please give me YOUR Cards") 
+return this.cards.splice(this.getLength() -numCards, numCards)
+}
+getLength(): number{
+return this.cards.length
+}
+readCard(card: Card): string{
+  let [suit, cardNumber] =card;
+return `${CardNumber[cardNumber]} of ${Suit[suit]}`
+}
+}
