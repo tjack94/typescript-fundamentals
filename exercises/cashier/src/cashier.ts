d4ae76f3c85a35b3d8eb120f65a70f55551e01c8
Taylor Jackson <tjack94@hotmail.com>
2018-11-13 21:36:15 -0700

    completed exercise four

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,3 +1,38 @@
-// export function cashier() {
-//   return {};
-// }
+interface CartItem{
+   name: string;
+   price: number;
+   qty: number;
+}
+interface CartAPI{
+  length: number;
+  total: number;
+  addItem(item: CartItem): CartAPI;
+  add(name: string, price: number, qty: number): CartAPI;
+}
+export function cashier():CartAPI {
+  let items: CartItem[]=[]
+  return {
+    get length() {
+      return items.reduce((acc, cur)=> {
+        return cur.qty + acc
+      }, 0)
+    },
+   get total(){
+    return items.reduce((acc, cur)=> {
+      return (cur.qty * cur.price) + acc
+    }, 0)
+   },
+    addItem(item): CartAPI{
+      items.push(item)
+      return this
+    },
+    add(name, price, qty=1): CartAPI{
+      items.push({
+        name,
+        price, 
+        qty
+      })
+      return this
+    }
+  };
+}
